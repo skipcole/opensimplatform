24c2603a12413e24578713fcb26dfadf12336b8f
skip.cole@gmail.com <skip.cole@gmail.com@18b51368-8a5c-11dd-aa02-45bbecb07bf0>
2010-07-02 12:07:19 +0000

    Worked on adding section to allow author and instructors to leave tips.

--- trunk/WEB-INF/classes/org/usip/osp/communications/Tips.java
+++ trunk/WEB-INF/classes/org/usip/osp/communications/Tips.java
@@ -1,8 +1,12 @@
 package org.usip.osp.communications;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.*;
 
 import org.hibernate.annotations.Proxy;
+import org.usip.osp.persistence.MultiSchemaHibernateUtil;
 
 
 /**
@@ -24,4 +28,182 @@ import org.hibernate.annotations.Proxy;
 @Proxy(lazy = false)
 public class Tips {
 
+	/** Database id of this Tip. */
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Pulls the tips out of the database base on its id and schema.
+	 * 
+	 * @param schema
+	 * @param sim_id
+	 * @return
+	 */
+	public static Tips getMe(String schema, Long sim_id) {
+
+		MultiSchemaHibernateUtil.beginTransaction(schema);
+		Tips tips = (Tips) MultiSchemaHibernateUtil.getSession(schema).get(Tips.class, sim_id);
+
+		MultiSchemaHibernateUtil.commitAndCloseTransaction(schema);
+
+		return tips;
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<Tips> getChildren(Long parentId, String schema) {
+
+		MultiSchemaHibernateUtil.beginTransaction(schema);
+
+		List<Tips> returnList = MultiSchemaHibernateUtil.getSession(schema).createQuery(
+				"from Tips where parentTipTextId = :sim_id ")
+				.setString("sim_id", parentId.toString())
+				.list(); //$NON-NLS-1$
+
+		MultiSchemaHibernateUtil.commitAndCloseTransaction(schema);
+
+		return returnList;
+	}
+	
+	public void saveMe(String schema) {
+
+		MultiSchemaHibernateUtil.beginTransaction(schema);
+
+		MultiSchemaHibernateUtil.getSession(schema).saveOrUpdate(this);
+
+		MultiSchemaHibernateUtil.commitAndCloseTransaction(schema);
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<Tips> getAllForSim(Long simid, String schema) {
+
+		MultiSchemaHibernateUtil.beginTransaction(schema);
+
+		List<Tips> returnList = MultiSchemaHibernateUtil.getSession(schema).createQuery(
+				"from Tips where simId = :sim_id ")
+				.setString("sim_id", simid.toString())
+				.list(); //$NON-NLS-1$
+
+		MultiSchemaHibernateUtil.commitAndCloseTransaction(schema);
+
+		return returnList;
+	}
+	
+	private Long parentTipTextId;
+	private Long simId;
+	private Long runningSimId;
+	private Long actorId;
+	private Long phaseId;
+	
+	private boolean isInstructorAdded;
+	
+	private String instructorsName;
+	private String instructorsId;
+	private String instructorsEmail;
+	
+	private String tipText;
+	
+	private Date tipLastEditDate;
+
+
+	public Long getParentTipTextId() {
+		return parentTipTextId;
+	}
+
+	public void setParentTipTextId(Long parentTipTextId) {
+		this.parentTipTextId = parentTipTextId;
+	}
+
+	public Long getSimId() {
+		return simId;
+	}
+
+	public void setSimId(Long simId) {
+		this.simId = simId;
+	}
+
+	public Long getRunningSimId() {
+		return runningSimId;
+	}
+
+	public void setRunningSimId(Long runningSimId) {
+		this.runningSimId = runningSimId;
+	}
+
+	public Long getActorId() {
+		return actorId;
+	}
+
+	public void setActorId(Long actorId) {
+		this.actorId = actorId;
+	}
+
+	public Long getPhaseId() {
+		return phaseId;
+	}
+
+	public void setPhaseId(Long phaseId) {
+		this.phaseId = phaseId;
+	}
+
+	public boolean isInstructorAdded() {
+		return isInstructorAdded;
+	}
+
+	public void setInstructorAdded(boolean isInstructorAdded) {
+		this.isInstructorAdded = isInstructorAdded;
+	}
+
+	public String getInstructorsName() {
+		return instructorsName;
+	}
+
+	public void setInstructorsName(String instructorsName) {
+		this.instructorsName = instructorsName;
+	}
+
+	public String getInstructorsId() {
+		return instructorsId;
+	}
+
+	public void setInstructorsId(String instructorsId) {
+		this.instructorsId = instructorsId;
+	}
+
+	public String getInstructorsEmail() {
+		return instructorsEmail;
+	}
+
+	public void setInstructorsEmail(String instructorsEmail) {
+		this.instructorsEmail = instructorsEmail;
+	}
+
+	public String getTipText() {
+		return tipText;
+	}
+
+	public void setTipText(String tipText) {
+		this.tipText = tipText;
+	}
+
+	public Date getTipLastEditDate() {
+		return tipLastEditDate;
+	}
+
+	public void setTipLastEditDate(Date tipLastEditDate) {
+		this.tipLastEditDate = tipLastEditDate;
+	}
+	
+	
 }
